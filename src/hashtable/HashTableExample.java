f8ca256f701cccb89a70f206df9d7dbf381e109d
elvisLutta <eolutta2@gmail.com>
2018-11-05 19:50:10 +0300

    New branch with linear probing hashtable.

--- src/hashtable/HashTableExample.java
+++ src/hashtable/HashTableExample.java
@@ -3,14 +3,15 @@ package hashtable;
 import java.util.Arrays;
 
 /**
- * Simple implementation of the Hashtable data structure.
+ * Simple implementation of the Hashtable data structure
+ * using linear probing to avoid collision.
  * For simplicity purposes we store integers.
  * We store the data in an array.
  */
 public class HashTableExample {
 
     private int arrSize = 100;
-
+    private int dataSize = 0;
     private int[] dataStore = new int[arrSize];
 
     private HashTableExample(int data) {
@@ -21,38 +22,56 @@ public class HashTableExample {
 
     /**
      * To hash we get the modulus of the input integer and the
-     * array size. If some data already exists in the position,
-     * keep checking the next until we find an empty spot or the
-     * we find data already exists or the counter is 100.
+     * array size.
+     *
      * @param data integer to be stored in the hashtable
      * @return int position where the data was stored on the array
      */
     private int hash(int data) {
-        if (data < 0)
-            data *= -1;  // convert negative numbers to positive
+        int hashcode = data % arrSize;
 
-        int counter1 = 0;
+        if (hashcode < 0)
+            hashcode *= -1;
+        return hashcode;
+    }
 
-        int mod = data % arrSize;
+    /**
+     * Method used to store data after hashcode is generated.
+     * If some data already exists in the position, keep checking
+     * the next until we find an empty spot or the we find
+     * data already exists or the counter is equal to arrSize.
+     *
+     * @param hashcode integer representing starting storage position
+     * @param data integer representing data to be stored
+     * @return position where data is stored.
+     */
+    private int store(int hashcode, int data) {
+//        Check if array is full and return -1
+        if (dataSize == arrSize - 1)
+            return -1;
 
-        if (dataStore[mod] == -1 || dataStore[mod] == data) {
-            dataStore[mod] = data;
-            return mod;
+        if (dataStore[hashcode] == -1 || dataStore[hashcode] == data) {
+            dataStore[hashcode] = data;
+            dataSize += 1;
+            return hashcode;
         }
 
-        int counter2 = mod + 1;
+        int counter1 = 0;
+        int counter2 = hashcode + 1;
 
         while (counter1 < arrSize) {
             if (counter2 > arrSize) {
                 counter2 = 0;
                 continue;
             }
+
             if (dataStore[counter2] == data) {
                 return counter2;
             }
 
             if (dataStore[counter2] == -1) {
                 dataStore[counter2] = data;
+                dataSize += 1;
                 return counter2;
             }
 
@@ -61,22 +80,50 @@ public class HashTableExample {
         }
 
         return -1;
+
     }
 
     /**
      * Hash the data and return true if data was added
      * or false otherwise.
+     *
      * @param data integer theat we want to store in the hashtable
      * @return boolean shows whether the data was stored successfully
      */
     public boolean put(int data) {
-        int res = hash(data);
-
-        return res > -1;
+        return store(hash(data), data) > -1;
     }
 
+    /**
+     * Method to check if data is contained in the hashtable and
+     * returns the specific position it's at.
+     *
+     * @param data The integer we are looking for in the hashtable
+     * @return position where data is located.
+     */
     public int get(int data) {
-        return hash(data);
+        int hashcode = hash(data);
+        int counter = 0;
+
+        while (counter < arrSize) {
+
+            if (dataStore[hashcode] == data)
+                return hashcode;
+
+            if (dataStore[hashcode] == -1)
+                return -1;
+
+            if (hashcode == arrSize - 1) {
+                hashcode = 0;
+                continue;
+            }
+
+            hashcode += 1;
+            counter += 1;
+
+        }
+
+        return -1;
     }
 
     public static void main(String[] args) {
@@ -89,6 +136,6 @@ public class HashTableExample {
         System.out.println("result 3 : " + ht.get(3));
         System.out.println("result 4 : " + ht.get(4));
         System.out.println("result 5 : " + ht.get(5));
-        System.out.println("result 103 : " + ht.get(103));
+        System.out.println("result 102 : " + ht.get(102));
     }
 }
