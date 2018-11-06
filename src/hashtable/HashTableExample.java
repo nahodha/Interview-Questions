54a229f72d4d480b562c07fc5220b69c22fd6bc5
Elvis <eolutta2@gmail.com>
2018-11-06 23:43:30 +0300

    Created new branch HashTableDoubleHashing for double hashing technique.

--- src/hashtable/HashTableExample.java
+++ src/hashtable/HashTableExample.java
@@ -25,11 +25,34 @@ public class HashTableExample {
      * @param data integer to be stored in the hashtable
      * @return int position where the data was stored on the array
      */
-    private int hash(int data) {
+    private int hash1(int data) {
         int hashcode = data % arrSize;
 
         if (hashcode < 0)
             hashcode *= -1;
+
+        return hashcode;
+    }
+
+    /**
+     * This method is called whenever there is a collision.
+     * It generates a new hash using the existing data and
+     * if the new hash is 0 it uses a different prime to
+     * generate the new prime.
+     *
+     * @param data integer to be stored and is used to generate hashes
+     * @return new hash integer
+     */
+    private int hash2(int data) {
+        int hashcode = 31 - data % arrSize;
+
+        if (hashcode == 0) {
+            hashcode = 37 - data % arrSize;
+        }
+
+        if (hashcode > arrSize - 1) {
+            hashcode %= arrSize;
+        }
 
         return hashcode;
     }
@@ -52,7 +75,7 @@ public class HashTableExample {
         int newHash;
 
         for (int i = 1; i < arrSize; i++) {
-            newHash = (hashcode + i * i) % arrSize;
+            newHash = (hashcode + i * hash2(data)) % arrSize;
 
             if (hashtable[newHash] != null) {
                 continue;
@@ -74,11 +97,11 @@ public class HashTableExample {
      * @return position where data was stored
      */
     public boolean put(int data) {
-        return store(hash(data), data) > -1;
+        return store(hash1(data), data) > -1;
     }
 
     public int get(int data) {
-        int hashcode = hash(data);
+        int hashcode = hash1(data);
 
         if (hashtable[hashcode] == null) {
             return -1;
@@ -91,7 +114,7 @@ public class HashTableExample {
         int newHash;
 
         for (int i = 1; i < arrSize; i++) {
-            newHash = (hashcode + i * i) % arrSize;
+            newHash = (hashcode + i * hash2(data)) % arrSize;
 
             if (hashtable[newHash] == null) {
                 return -1;
@@ -113,11 +136,13 @@ public class HashTableExample {
         ht.put(-4);
         ht.put(5);
         ht.put(10212);
+        ht.put(-5);
 
         System.out.println("3 located at position " + ht.get(3));
         System.out.println("-4 located at position " + ht.get(-4));
         System.out.println("5 located at position " + ht.get(5));
         System.out.println("10212 located at position " + ht.get(10212));
         System.out.println("-23 located at position " + ht.get(-23));
+        System.out.println("-5 located at position " + ht.get(-5));
     }
 }
